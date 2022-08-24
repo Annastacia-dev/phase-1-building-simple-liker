f3a568cb50ee1b4e7d55c1ced4695f6ad5372f45
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-24 09:51:15 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButton = document.querySelectorAll(".like-glyph");
+
+function likeAction(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then (() => {
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART;
+      heart.className = 'activated-heart';
+    }else {
+      heart.innerText = EMPTY_HEART
+      heart.className = '';
+    }
+  })
+  .catch((error) => {
+    const modal = document.querySelector('#modal');
+    modal.className = '';
+    modal.innerText = error;
+    setTimeout(() => modal.className = 'hidden', 3000)
+
+  });
+}
+
+for (const glyph of likeButton){
+  glyph.addEventListener ('click', likeAction)
+}
 
 
 
